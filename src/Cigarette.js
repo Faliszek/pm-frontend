246e2581c42775d1e4ecfc01b978d8b7e03bd1fe
Paweł Falisz <pawlic7@gmail.com>
2019-09-14 15:35:22 +0200

    create deafult components

---
+++ src/Cigarette.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Cigarette(props) {
+  return <div>C</div>;
+}
---
+++ src/Water.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Water() {
+  return <div>Water</div>;
+}
