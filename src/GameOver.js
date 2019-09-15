cdf4b7674754cb63ce155b509c65cfa09e105698
Paweł Falisz <pawlic7@gmail.com>
2019-09-15 09:51:13 +0200

    10 points to win

--- src/GameOver.js
+++ src/GameOver.js
@@ -8,6 +8,6 @@ import { Failure } from "./Failure";
 export function GameOver(props) {
   const game = React.useContext(GameStateContext);
 
-  return <div>{game.state.score >= 50 ? <Success /> : <Failure />}</div>;
+  return <div>{game.state.score >= 10 ? <Success /> : <Failure />}</div>;
 }
 //Zdobylem 50 pkt, nie zdobyl
